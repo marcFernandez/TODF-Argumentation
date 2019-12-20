11845fa4a8460033813ec350b0e2a52aa0ba8997
marcFernandez <markiff@gmail.com>
2019-12-20 01:24:25 +0100

    working
    Signed-off-by: Marc Fernández <marc.fernandez@enzymeadvisinggroup.com>

--- jsonifier.py
+++ jsonifier.py
@@ -15,6 +15,8 @@ if __name__ == "__main__":
 
     comments_dict = {}
 
+    it = 0
+
     pbar = ProgressBar()
 
     directory = "/Users/demo/Documents/metadecidim-master/comments/"
@@ -22,6 +24,8 @@ if __name__ == "__main__":
 
     last_prop = "08584"
     changeProp = False
+
+    most_commented_array = []
 
     for filename in pbar(os.listdir(directory)):
         curr_prop = filename[:5]
@@ -58,9 +62,25 @@ if __name__ == "__main__":
                 "total_votes": curr_prop_metadata["proposal"]["total_votes"]
             }
 
-        if curr_prop != last_prop and curr_prop != '00000':
-            output_file = 'prop_' + last_prop + '.json'
-            with open(output_file, 'w') as outfile:
-                json.dump(comments_dict[last_prop], outfile, indent=4, sort_keys=True)
-                outfile.close()
-            last_prop = curr_prop
+        # if curr_prop != last_prop and curr_prop != '00000':
+        #     last_prop = curr_prop
+        #     if len(comments_dict[last_prop]["comments"]) > 25 and last_prop != '08584' and curr_prop != '08584':
+        #         it += 1
+        #         output_file = 'Prop_' + last_prop + '.json'
+        #         with open(output_file, 'w') as outfile:
+        #             json.dump(comments_dict[last_prop], outfile, indent=4, sort_keys=True)
+        #             outfile.close()
+                # last_prop = curr_prop
+
+    amount = 0
+    n = 40
+    arr = []
+    for k in comments_dict.keys():
+        if len(comments_dict[k]["comments"]) > n:
+            amount += 1
+            arr.append(k)
+            # print k
+    arr.sort()
+    for i in arr:
+        print i
+    print "There are "+str(amount)+" proposals with more than "+str(n)+" comments."
